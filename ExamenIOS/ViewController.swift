756d021c1a92065de6f95b47dd87d819ca8889bc
lady.program <nymeriastark74@gmail.com>
2025-06-17 09:53:23 +0200

    Initial Commit

---
+++ ExamenIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenIOS
+//
+//  Created by Mañanas on 17/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExamenIOSTests/ExamenIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenIOSTests.swift
+//  ExamenIOSTests
+//
+//  Created by Mañanas on 17/6/25.
+//
+
+import Testing
+@testable import ExamenIOS
+
+struct ExamenIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
